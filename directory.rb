d827fc74f482b20d172ec0a03fd8d12d6fa6cf6b
Declan <declankolakowski@gmail.com>
2020-03-30 13:57:03 +0000

    refactored code for user input

--- directory.rb
+++ directory.rb
@@ -17,8 +17,8 @@ def print_header
   puts "-------------"
 end
 
-def print(student_list)
-  student_list.each do |student|
+def print(students)
+  students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -27,6 +27,25 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
 
+def input_students
+  puts "Please enter the names of the students."
+  puts "to finish, just hit return twice."
+  
+  students = []
+  
+  name = gets.chomp
+  
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "There are now #{students.count} students"
+    name = gets.chomp
+  end
+  
+  return students
+  
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
