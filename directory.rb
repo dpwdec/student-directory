a7faebefb80c16b3c53fef8718fd17c4970b2186
Declan <declankolakowski@gmail.com>
2020-03-31 15:28:18 +0000

    added student cohort grouping options

--- directory.rb
+++ directory.rb
@@ -126,68 +126,6 @@ def try_load_file(filename)
   end
 end
 
-def print_menu_options
-  puts_multiple("-------------------------",
-  "1: Input student information.",
-  "2: Show student information.",
-  "3: Save list of students.",
-  "4: Load list of students.",
-  "5: Search students by first name letter",
-  "6: Search students by name length",
-  "9: Exit directory.")
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      choose_save_file
-    when "4"
-      choose_load_file
-    when "5"
-      choose_student_letter
-    when "6"
-      choose_student_name_length
-    when "9"
-      exit
-    else
-      puts "Invalid option."
-  end
-end
-
-#root management for user input
-def interactive_menu
-  loop do
-    print_menu_options
-    process(STDIN.gets.chomp)
-  end
-end
-
-
-
-#students = input_students
-#puts ARGV.inspect
-
-#print_students(students)
-
-#print_students_by_letter(students, "j")
-#print_students_names_shorter_than_n(students, 12)
-
-#print_header
-#print(students)
-#print_footer(students)
-
-# -----------------------------------
-
 def choose_student_letter
   puts_multiple("-----------------------",
   "Please type the first letter of names of students you wish to list.",
@@ -229,32 +167,72 @@ def print_students_names_shorter_than_n(name_length=10)
   end
 end
 
-def print_students_by_cohort(students)
-  
+def print_students_by_cohort
   cohorts = Hash.new
-  
-  students.each do |student|
+  @students.each do |student|
     if cohorts.has_key?(student[:cohort])
       cohorts[student[:cohort]] << student[:name]
     else
       cohorts[student[:cohort]] = [student[:name]]
     end
   end
-  
   cohorts.each do |key, value|
-    
     puts "#{key} cohort students:"
-    
     value.each do |student|
       puts "#{student}"
     end
-    
     puts "----------------------------"
-    
   end
-  
 end
 
+def print_menu_options
+  puts_multiple("-------------------------",
+  "1: Input student information.",
+  "2: Show student information.",
+  "3: Save list of students.",
+  "4: Load list of students.",
+  "5: Search students by first name letter.",
+  "6: Search students by name length.",
+  "7: Display students ordered by cohort.",
+  "8: Exit directory.")
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      choose_save_file
+    when "4"
+      choose_load_file
+    when "5"
+      choose_student_letter
+    when "6"
+      choose_student_name_length
+    when "7"
+      print_students_by_cohort
+    when "9"
+      exit
+    else
+      puts "Invalid option."
+  end
+end
+
+#root management for user input
+def interactive_menu
+  loop do
+    print_menu_options
+    process(STDIN.gets.chomp)
+  end
+end
 
 try_load_students
 interactive_menu
